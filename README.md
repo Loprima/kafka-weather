e14231af98a2ef3e793113303df32a733b5f6fb9
Loprima <lomovnik1@gmail.com>
2024-12-23 20:24:35 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # kafka-weather
+
+## Запуск:
+
+- 1. Запустить docker desktop
+
+- 2. Compose Up docker-compose.yaml
+
+- 3. Запустить скрипт producer.py
+
+- 4. Запустить скрипт consumer.py
--- producer.py
+++ producer.py
@@ -7,7 +7,7 @@ from kafka import KafkaProducer
 cities = ["Moscow", "Saint Petersburg", "Krasnoyarsk", "Sochi", "Vladivostok"]
 conditions = ["sunny", "rainy", "cloudy", "snowy", "windy"]
 
-# Инициализация продюсера Kafka
+# Инициализация продюсера
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
@@ -19,12 +19,12 @@ try:
         city = random.choice(cities)
         weather_data = {
             "city": city,
-            "temperature": random.randint(-30, 35),
+            "temperature": random.randint(-40, 40),
             "condition": random.choice(conditions)
         }
         producer.send("weather", value=weather_data)
         print(f"Sent: {weather_data}")
-        time.sleep(1)  # Задержка для имитации реального времени
+        time.sleep(1)  # Задержка чтобы не улететь
 except KeyboardInterrupt:
     print("Producer stopped.")
 finally:
